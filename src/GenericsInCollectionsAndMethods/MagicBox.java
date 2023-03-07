b1cbb9ea3ffd4a4ef8e6547ecc6a0abd4d0de00c
jdvpgii <gii.jdvp@gmail.com>
2023-03-07 21:37:39 +0500

    #1

---
+++ src/GenericsInCollectionsAndMethods/MagicBox.java
@@ -0,0 +1,38 @@
+package GenericsInCollectionsAndMethods;
+
+import java.util.Random;
+
+public class MagicBox<T> {
+    Random random = new Random();
+    T[] item;
+    public MagicBox(int howManyObjects) {
+        item = (T[]) new Object[howManyObjects];
+    }
+
+    public boolean add(T items) {
+        for (int i = 0; i < item.length; i++) {
+            if (item[i] == null) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public T pick() {
+        int randomRange = random.nextInt(item.length);
+        T obj = item[randomRange];
+        int nullCounter = 0;
+
+        for (int i = 0; i < item.length; i++) {
+            if (item[i] == null) {
+                nullCounter++;
+            }
+        }
+
+        if (nullCounter != 0) {
+            throw new RuntimeException("Коробка не полная, осталось заполнить еще " + nullCounter + " ячеек");
+        } else {
+            return obj;
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import GenericsInCollectionsAndMethods.MagicBox;
+
 public class Main {
     public static void main(String[] args) {
+        MagicBox<Object> magicBox1 = new MagicBox<>(111);
 
+        String text = new String("some tetx");
+
+//        magicBox1.add(33);
+//        for (int i = 0; i < 11; i++) {
+//            magicBox1.add(text);
+//        }
+        magicBox1.pick();
     }
 }
