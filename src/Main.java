c8923f8d0855bcd282e13b87a47a0b7cd89a4ba1
jdvpgii <gii.jdvp@gmail.com>
2023-03-12 17:56:38 +0500

    #1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
 
+
+        while (true) {
+            System.out.println("\tSELECT OPTION\n" +
+                    "1. Add\t2.Show\t3.Show\n" +
+                    "============================\n");
+
+        }
     }
 }
