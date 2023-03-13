f3190fab4b2bdc33bec8f16cce48f15e7678d22d
jdvpgii <gii.jdvp@gmail.com>
2023-03-13 23:51:19 +0500

    #2

--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,21 @@
 public class Person {
+    private String firstName;
+    private int tickets;
 
+    public Person(String firstName, int tickets) {
+        this.firstName = firstName;
+        this.tickets = tickets;
+    }
+
+    public int getTickets() {
+        return tickets;
+    }
+
+    public void setTickets(int tickets) {
+        this.tickets = tickets;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
 }
