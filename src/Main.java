8d93847aeae4f993cfd3c18ed3ac64852a13016d
jdvpgii <gii.jdvp@gmail.com>
2023-03-16 00:11:31 +0500

    #1

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,20 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+
 public class Main {
+    private HashMap<Character, Integer> letters = new HashMap<>();
+    private static String lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.   ";
+
+    public HashMap<Character, Integer> getLetters() {
+        return letters;
+    }
+
+    public static String getLorem() {
+        return lorem;
+    }
+
     public static void main(String[] args) {
 
     }
