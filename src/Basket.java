0e498836f0c9fc9329f6a4e413f31492337d1147
jdvpgii <gii.jdvp@gmail.com>
2023-04-15 01:32:43 +0500

    IO_File_Serialization#1

---
+++ src/Basket.java
@@ -0,0 +1,37 @@
+import java.io.File;
+
+public class Basket {
+    //конструктор, принимающий массив цен и названий продуктов;
+    public Basket() {
+    }
+
+    //метод добавления amount штук продукта номер productNum в корзину;
+    void addToCart(int productNum, int amount){
+
+    }
+
+    //метод вывода на экран покупательской корзины
+    void printCart() {
+
+    }
+
+    //метод сохранения корзины в текстовый файл; использовать встроенные сериализаторы нельзя;
+    void saveTxt() {
+
+    }
+
+    //статический(!) метод восстановления объекта корзины из текстового файла, в который ранее была она сохранена;
+    static Basket loadFromTxtFile(File textFile) {
+        return new Basket();
+    }
+
+    //для сохранения в файл в бинарном формате.
+    void saveBin(File file) {
+
+    }
+    // для загрузки корзины из бинарного файла.
+    static Basket loadFromBinFile(File file) {
+        return new Basket();
+    }
+
+}
