595d24cf3cfcd86f129cf126860a6eb1525f0386
jdvpgii <gii.jdvp@gmail.com>
2023-05-22 20:43:27 +0500

    MavenLesson#2

---
+++ MavenLesson/api/src/main/java/Main.java
@@ -0,0 +1,12 @@
+import ru.gii.netology.MyEntity;
+import ru.gii.netology.MyService;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MyService myService = new MyService();
+        System.out.println(myService.getMyEntity());
+        System.out.println(myService.setMyEntity(new MyEntity("second")));
+        System.out.println(myService.getMyEntity());
+    }
+}
